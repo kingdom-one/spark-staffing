09bb7e419ab509154f5497c4b2b7087fcb3c9c27
Joontae93 <kj.roelke@gmail.com>
2022-01-18 16:45:14 -0600

    job package bug fix

--- src/modules/postAJob.js
+++ src/modules/postAJob.js
@@ -1,11 +1,19 @@
 export function packageSelector() {
 	const jobPackages = document.querySelectorAll('.job-package'),
 		jobPackageContainer = document.querySelector('.job_packages'),
+		userJobPackages = document.querySelectorAll('.user-job-package'),
+		userJobPackageContainer = document.querySelector('.user-job_packages'),
+		userRadioButtons = userJobPackageContainer.querySelectorAll('input'),
 		radioButtons = jobPackageContainer.querySelectorAll('input');
 
 	jobPackages.forEach((job, i) =>
 		job.addEventListener('click', () => {
 			radioButtons[i].checked = true;
+		}),
+	);
+	userJobPackages.forEach((job, i) =>
+		job.addEventListener('click', () => {
+			userRadioButtons[i].checked = true;
 		}),
 	);
 }
