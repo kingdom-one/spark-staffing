813a4d08eb4ab743c472bb8cfb1e1c1215ae19ac
Joontae93 <kj.roelke@gmail.com>
2022-01-20 16:29:46 -0600

    update woocommerce checkout fields for guest

--- inc/buddypress__filters.php
+++ inc/buddypress__filters.php
@@ -11,11 +11,9 @@ add_filter('bp_template_hierarchy_members_single_item', function ($templates) {
             array_unshift($templates, "members/single/index-{$member_type}.php");
         };
     }
-    // else {
-    // array_unshift($templates, "members/single/index-subscriber.php");
-    // }
     return $templates;
 });
+
 /**
  * Limit how many memebers should be shown per page
  */
@@ -24,8 +22,6 @@ add_filter('bp_template_hierarchy_members_single_item', function ($templates) {
 //     $args['per_page'] = 50;
 //     return $args;
 // });
-
-
 
 /**
  * Define Avatar Size
@@ -59,6 +55,11 @@ add_filter('bp_before_has_profile_parse_args', 'spark_exclude_profile_field_grou
  * @param {array} $args - field IDs to suppress
  */
 function skip_church_xprofile_fields($args) {
+    // hide account type
+    if (!is_admin()) {
+        $args['exclude_fields'] = array(63);
+        return $args;
+    }
     // display if editing profile
     if (bp_is_user_profile_edit() || is_admin()) return $args;
     if (bp_is_user_profile()) {
@@ -66,84 +67,50 @@ function skip_church_xprofile_fields($args) {
         return $args;
     }
 }
-// add_filter('bp_before_has_profile_parse_args', 'skip_church_xprofile_fields');
+add_filter('bp_before_has_profile_parse_args', 'skip_church_xprofile_fields');
 
-function removeDumbRedirects() {
-    // apply_filters('wc4bp_activate_stream_activity', true);
-    remove_action('template_redirect', 'wc4bp_redirect_to_profile');
-    remove_filter('page_link', 'wc4bp_page_link_router', 9999, 2);
-    remove_filter('wc4bp_checkout_page_link', 'home');
-    $wc4bp_options = get_option('wc4bp_options');
-    $wc4bp_pages_options = get_option('wc4bp_pages_options');
-    $wc4bp_options['tab_cart_disabled'] = true;
-    var_dump($wc4bp_pages_options);
+
+
+/** Only filter fields for new users */
+if (!is_user_logged_in()) {
+    add_filter('woocommerce_checkout_fields', 'custom_override_checkout_fields');
+    add_action('woocommerce_form_field_text', 'account_settings_heading', 10, 2);
 }
-// removeDumbRedirects();
-/**
- * Add BudyPress fields to the WooCommerce checkout page by using the xprofile_get_field function from BudyPress
- *
- * @see xprofile_get_field()
- */
-function addBuddyPressToWooCommerceCheckout($checkout_fields) {
-    // text field example
-    $bp_field          = xprofile_get_field(1); // Get BudyPress field with Field ID
-    $bp_field_wc_id    = sanitize_title('bp_field_' .  $bp_field->name); // Field ID to identify through the WooCommerce checkout process
-    $checkout_fields['billing'][$bp_field_wc_id] = [
-        'type'        => 'text',
-        'class'       => ['form-row-wide', 'budypress-field', 'budypress-field-' . $bp_field->id],
-        'label'       => $bp_field->name,
-        'placeholder' => '',
-        'required'    => $bp_field->is_required,
-        'default'     => '',
-        'description'     => $bp_field->description,
-    ];
-    foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-        $product_id = $cart_item['product_id'];
+
+function custom_override_checkout_fields($fields) {
+    unset($fields['order']['order_comments']);
+    $fields['account']    = array(
+        'account_username' => array(
+            'type' => 'text',
+            'label' => __('Username', 'woocommerce'),
+            'description' => __('This will be your profile slug (e.g. sparkstaffing.co/profile/username)', 'woocommerce'),
+            'placeholder' => _x('Username', 'placeholder', 'woocommerce'),
+            'required' => true,
+        ),
+        'account_password' => array(
+            'type' => 'password',
+            'label' => __('Account password', 'woocommerce'),
+            'placeholder' => _x('Password', 'placeholder', 'woocommerce'),
+            'class' => array('form-row-first'),
+            'required' => true,
+        ),
+        'account_password-2' => array(
+            'type' => 'password',
+            'label' => __('Verify password', 'woocommerce'),
+            'placeholder' => _x('Password', 'placeholder', 'woocommerce'),
+            'class' => array('form-row-last'),
+            'label_class' => array('hidden'),
+            'required' => true,
+        )
+    );
+    return $fields;
+}
+
+
+function account_settings_heading($field, $key) {
+    // will only execute if the field is account_username and we are on the checkout page...
+    if (is_checkout() && ($key == 'account_username')) {
+        $field = '<h2 class="form-row form-row-wide">Account Details</h2>' . $field;
     }
-    // select box example
-    $bp_field         = xprofile_get_field(63); // Get BudyPress field with Field ID
-    $bp_field_wc_id   = sanitize_title('bp_field_' . $bp_field->name); // Field ID to identify through the WooCommerce checkout process
-    $bp_field_options = [
-        'option_1' => 'Candidate',
-        'option_2' => 'Church',
-    ]; // select box options
-    foreach ($bp_field->get_children() as $option) $bp_field_options[$option->name] = $option->name; // Insert options from BuddyPress select box field
-    $checkout_fields['billing'][$bp_field_wc_id] = [
-        'type'     => 'radio',
-        'checked'     => 'checked',
-        'class'    => ['form-row-wide', 'budypress-field', 'budypress-field-' . $bp_field->id],
-        'label'    => $bp_field->name,
-        'required' => $bp_field->is_required,
-        'options'  => $bp_field_options,
-    ];
-    //     if ($product_id = 398) { // church
-
-    //     } 
-    //         if ($product_id = 396) { // candidate
-    // $checkout_fields['billing'][$bp_field_wc_id]['options'] = 
-    //         }
-
-    return $checkout_fields;
-}
-// if (!is_user_logged_in()) {
-//     add_filter('woocommerce_checkout_fields', 'addBuddyPressToWooCommerceCheckout');
-// }
-
-// // echo '<script>console.log("hello there, ' . $customer_id . '")</script>';
-// add_action('woocommerce_checkout_update_user_meta', 'syncWooCommerceAndBuddyPress', 10, 2);
-
-/**
- * After checkout completion, sync and update the BuddyPress fields for the customer 
- *
- * @see xprofile_get_field()
- */
-function syncWooCommerceAndBuddyPress($customer_id, $data) {
-    // Update text field example
-    $bp_field       = xprofile_get_field('211'); // Get BudyPress field by Field ID
-    $bp_field_wc_id = sanitize_title('bp_field_' . $bp_field->name); // Field ID to identify through the WooCommerce checkout process
-    if (isset($data[$bp_field_wc_id])) xprofile_set_field_data($bp_field->id, $customer_id, $data[$bp_field_wc_id]);
-    // Update select box example
-    $bp_field       = xprofile_get_field('958'); // Get BudyPress field by Field ID
-    $bp_field_wc_id = sanitize_title('bp_field_' . $bp_field->name); // Field ID to identify through the WooCommerce checkout process
-    if (isset($data[$bp_field_wc_id])) xprofile_set_field_data($bp_field->id, $customer_id, $data[$bp_field_wc_id]);
+    return $field;
 }
