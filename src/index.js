cee5707f432ca315aa45451638030b0e2e38b577
Joontae93 <kj.roelke@gmail.com>
2022-01-20 10:32:36 -0600

    refactor profileScripts into class

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 import '../scss/bp-spark.scss';
-import { socials } from './modules/profileScripts';
+
 import Footer from './modules/footerScripts';
 import Header from './modules/headers';
 import { sparkFeaturedJob } from './modules/featuredJob';
 import { packageSelector } from './modules/postAJob';
 import { replacedRequiredTag, controlAsterisk } from './modules/requiredFields';
 import highlightApplications from './modules/jobApplications';
+import { ProfileScripts } from './modules/profileScripts';
 function init() {
 	const footer = new Footer();
 	const header = new Header();
 	let URL = window.location.href;
+	const profileScipts = new ProfileScripts();
 	replacedRequiredTag();
-	if (URL.includes('checkout')) {
-		setTimeout(controlAsterisk, 500);
-	}
-	if (URL.includes('profile')) {
-		socials();
-	}
-	if (URL.includes('jobs')) {
-		setTimeout(sparkFeaturedJob, 2500);
-	}
-	if (URL.includes('post-a-job')) {
-		packageSelector();
-	}
-	if (URL.includes('job-dashboard')) {
-		highlightApplications();
-	}
+	if (URL.includes('checkout')) setTimeout(controlAsterisk, 500);
+	if (URL.includes('profile')) profileScipts.socials();
+	if (URL.includes('profile') && URL.includes('shop'))
+		profileScipts.hideWooNav();
+	if (URL.includes('jobs')) setTimeout(sparkFeaturedJob, 2500);
+	if (URL.includes('post-a-job')) packageSelector();
+	if (URL.includes('job-dashboard')) highlightApplications();
 }
 init();
--- src/modules/profileScripts.js
+++ src/modules/profileScripts.js
@@ -1,50 +1,59 @@
-/** The Goal: Replace div's p > a with a > svg
- * Step 1: Grab Elements
- * Step 2: Store HTML and Clear it from markup
- * Step 3: Create HTML inside Div
- * Step 4: Apply Styles
- */
-
 import { socialIcons } from './abstracts.js';
 
-/** Swap URLs for branded Icons */
-export function socials() {
-	// Step 1: Grab Elements
-	const socialContainer = document.querySelectorAll('.social__container'); // the div
-	const socialMediaSection = document.querySelector('.social-media'); // the Parent
-	// Step 2: Store HTML, then Erase
-	socialContainer.forEach((el) => {
-		const anchor = el.querySelector('a'); // full anchor element
-		const link = anchor.innerHTML; // url
-		el.innerHTML = '';
-		// Step 3:  Create HTML
-		if (anchor.innerText.includes('facebook')) swapHTML(link, el, 'Facebook');
-		if (anchor.innerText.includes('twitter')) swapHTML(link, el, 'Twitter');
-		if (anchor.innerText.includes('instagram')) swapHTML(link, el, 'Instagram');
-		if (anchor.innerText.includes('tiktok')) swapHTML(link, el, 'TikTok');
-		if (anchor.innerText.includes('pinterest')) swapHTML(link, el, 'Pinterest');
-		if (anchor.innerText.includes('vimeo')) swapHTML(link, el, 'Vimeo');
-		if (anchor.innerText.includes('youtube')) swapHTML(link, el, 'Youtube');
-		if (anchor.innerText.includes('linkedin')) swapHTML(link, el, 'Linkedin');
+export class ProfileScripts {
+	constructor() {
+		this.nav = document.querySelector(
+			'nav.bp-profile__subnav.item-list-tabs.no-ajax',
+		);
+		this.socialContainer = document.querySelectorAll('.social__container'); // the div
+		this.socialMediaSection = document.querySelector('.social-media'); // the Parent
+		// Step 2: Store HTML, then Erase
+	}
+	/** Swap URLs for branded Icons */
+	socials() {
+		// Step 1: Grab Elements
+		this.socialContainer.forEach((el) => {
+			const anchor = el.querySelector('a'); // full anchor element
+			const link = anchor.innerHTML; // url
+			el.innerHTML = '';
+			// Step 3:  Create HTML
+			if (anchor.innerText.includes('facebook'))
+				this._swapHTML(link, el, 'Facebook');
+			if (anchor.innerText.includes('twitter'))
+				this._swapHTML(link, el, 'Twitter');
+			if (anchor.innerText.includes('instagram'))
+				this._swapHTML(link, el, 'Instagram');
+			if (anchor.innerText.includes('tiktok'))
+				this._swapHTML(link, el, 'TikTok');
+			if (anchor.innerText.includes('pinterest'))
+				this._swapHTML(link, el, 'Pinterest');
+			if (anchor.innerText.includes('vimeo')) this._swapHTML(link, el, 'Vimeo');
+			if (anchor.innerText.includes('youtube'))
+				this._swapHTML(link, el, 'Youtube');
+			if (anchor.innerText.includes('linkedin'))
+				this._swapHTML(link, el, 'Linkedin');
 
-		// Step 4: Apply Styles
-		socialMediaSection.querySelector(
-			'.bp-profile__section--content',
-		).style.display = 'flex';
-		socialMediaSection.querySelector(
-			'.bp-profile__section--content',
-		).style.flexWrap = 'wrap';
-	});
-}
-
-/** Generate HTML with branded SVG
- * @param {string} link - the link to href
- * @param {Node} el - the `div` to rewrite in
- * @param {string} platform - the name of the platform (must be capitalized to match `socialIcons` object key)
- */
-function swapHTML(link, el, platform) {
-	const icon = socialIcons.findIndex((el) => el.platform === platform);
-	el.innerHTML = `<a href="${link}" target="_blank" class="social--link" >${socialIcons[icon].icon}</a>`;
-	const svg = el.querySelector('svg');
-	svg.style.fill = `${socialIcons[icon].brand}`;
+			// Step 4: Apply Styles
+			this.socialMediaSection.querySelector(
+				'.bp-profile__section--content',
+			).style.display = 'flex';
+			this.socialMediaSection.querySelector(
+				'.bp-profile__section--content',
+			).style.flexWrap = 'wrap';
+		});
+	}
+	hideWooNav() {
+		this.nav.style.display = 'none';
+	}
+	/** Generate HTML with branded SVG
+	 * @param {string} link - the link to href
+	 * @param {Node} el - the `div` to rewrite in
+	 * @param {string} platform - the name of the platform (must be capitalized to match `socialIcons` object key)
+	 */
+	_swapHTML(link, el, platform) {
+		const icon = socialIcons.findIndex((el) => el.platform === platform);
+		el.innerHTML = `<a href="${link}" target="_blank" class="social--link" >${socialIcons[icon].icon}</a>`;
+		const svg = el.querySelector('svg');
+		svg.style.fill = `${socialIcons[icon].brand}`;
+	}
 }
