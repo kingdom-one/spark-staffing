dad1eb49e07d7fc8fc46266cf048e0669779599a
Joontae93 <kj.roelke@gmail.com>
2022-02-04 15:07:22 -0600

    comment out unused code for better build

--- src/modules/headers.js
+++ src/modules/headers.js
@@ -24,21 +24,21 @@ export default class Header {
 			link.addEventListener('click', (e) => e.preventDefault());
 		});
 	}
-	toggleCart() {
-		const cart = document.querySelector('.widget_shopping_cart_content');
-		const cartToggle = document.getElementById('cartToggle');
-		const addToCart = document.querySelector('.add_to_cart_button');
-		console.log(cart);
-		console.log(cart.children.length);
-		for (let i = 0; i < cart.children.length; i++) {
-			console.log(cart.children[i].tagName);
-		}
-		if (cart.children.length === 0) {
-			cartToggle.style.display = 'none';
-		}
-		addToCart.addEventListener('click', () => {
-			if (!cartToggle.style.display === 'none') return;
-			cartToggle.style.display = 'inline-flex';
-		});
-	}
+	// toggleCart() {
+	// 	const cart = document.querySelector('.widget_shopping_cart_content');
+	// 	const cartToggle = document.getElementById('cartToggle');
+	// 	const addToCart = document.querySelector('.add_to_cart_button');
+	// 	console.log(cart);
+	// 	console.log(cart.children.length);
+	// 	for (let i = 0; i < cart.children.length; i++) {
+	// 		console.log(cart.children[i].tagName);
+	// 	}
+	// 	if (cart.children.length === 0) {
+	// 		cartToggle.style.display = 'none';
+	// 	}
+	// 	addToCart.addEventListener('click', () => {
+	// 		if (!cartToggle.style.display === 'none') return;
+	// 		cartToggle.style.display = 'inline-flex';
+	// 	});
+	// }
 }
