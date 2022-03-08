c01e51bc48b41f63be0d6b31f6d3dca172ffcb97
Joontae93 <kj.roelke@gmail.com>
2022-03-08 13:46:14 -0600

    update job listing and resume submission

--- single-job_listing.php
+++ single-job_listing.php
@@ -4,34 +4,32 @@
 <div class="x-main full <?php //x_main_content_class(); 
                         ?>" role="main">
     <?php while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <? global $post; ?>
-            <div class="single-job-listing" id="single-job-listing">
-                <div class="single-job-listing__header">
-                    <h1 class="single-job-listing--title">
-                        <? wpjm_the_job_title() ?>
-                    </h1>
-                </div>
-                <div class="job-description">
-                    <?php wpjm_the_job_description(); ?>
-                </div>
-                <div class="single-job-listing__meta">
-                    <div class="single-job-listing__meta--container">
-                        <?php if (get_option('job_manager_hide_expired_content', 1) && 'expired' === $post->post_status) : ?>
-                            <div class=" job-manager-info">
-                                <?php _e('This listing has expired.', 'wp-job-manager'); ?>
-                            </div>
-                        <?php else : ?>
-                            <?php do_action('single_job_listing_start'); ?>
-                    </div>
-                </div>
-                <?php
-                            if (candidates_can_apply()) get_job_manager_template('job-application.php');
-                            do_action('single_job_listing_end'); ?>
-            <?php endif; ?>
-
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <? global $post; ?>
+        <div class="single-job-listing" id="single-job-listing">
+            <div class="single-job-listing__header">
+                <h1 class="single-job-listing--title">
+                    <? wpjm_the_job_title() ?>
+                </h1>
             </div>
-        </article>
+            <div class="job-description">
+                <?php wpjm_the_job_description(); ?>
+            </div>
+            <div class="single-job-listing__meta">
+                <div class="single-job-listing__meta--container">
+                    <?php if (get_option('job_manager_hide_expired_content', 1) && 'expired' === $post->post_status) : ?>
+                    <div class=" job-manager-info">
+                        <?php _e('This listing has expired.', 'wp-job-manager'); ?>
+                    </div>
+                    <?php else :
+                            do_action('single_job_listing_start');
+                            do_action('single_job_listing_end');
+                        endif; ?>
+                </div>
+            </div>
+            <?php if (candidates_can_apply()) get_job_manager_template('job-application.php'); ?>
+        </div>
+    </article>
     <?php endwhile; ?>
 </div>
 <!-- <?php //get_sidebar(); 
--- wp-job-manager-resumes/resume-submit.php
+++ wp-job-manager-resumes/resume-submit.php
@@ -18,46 +18,35 @@ if (!defined('ABSPATH')) {
 
 wp_enqueue_script('wp-resume-manager-resume-submission');
 ?>
-<form action="<?php echo $action; ?>" method="post" id="submit-resume-form" class="job-manager-form"
-    enctype="multipart/form-data">
-    <?php do_action('submit_resume_form_start'); ?>
+<form action="<?php echo $action; ?>" method="post" id="submit-resume-form" class="job-manager-form" enctype="multipart/form-data">
+    <?php do_action('submit_resume_form_start');
+    if (apply_filters('submit_resume_form_show_signin', true)) {
+        get_job_manager_template('account-signin.php', ['class' => $class], 'wp-job-manager-resumes', RESUME_MANAGER_PLUGIN_DIR . '/templates/');
+    }
+    if (resume_manager_user_can_post_resume()) :
+        // Resume Fields 
+        $resume_fields['resume_content']['label'] = 'Resume Summary';
+        $resume_fields['candidate_education']['fields']['qualification']['label'] = 'Degree or Certification Name';
+        do_action('submit_resume_form_resume_fields_start');
+        foreach ($resume_fields as $key => $field) : ?>
+            <fieldset class="fieldset-<?php esc_attr_e($key); ?>">
+                <label for="<?php esc_attr_e($key); ?>"><?php echo $field['label'] . apply_filters('submit_resume_form_required_label', $field['required'] ? ' <img class="requiredAsterisk" src=' . wp_get_attachment_url(13) . '>' : ' <small>' . __('(Recommended)', 'wp-job-manager-resumes') . '</small>', $field); ?></label>
+                <div class="field">
+                    <?php $class->get_field_template($key, $field); ?>
+                </div>
+            </fieldset>
+        <?php endforeach; ?>
 
-    <?php if (apply_filters('submit_resume_form_show_signin', true)) : ?>
-
-    <?php get_job_manager_template('account-signin.php', ['class' => $class], 'wp-job-manager-resumes', RESUME_MANAGER_PLUGIN_DIR . '/templates/'); ?>
-
-    <?php endif; ?>
-
-    <?php if (resume_manager_user_can_post_resume()) : ?>
-
-    <!-- Resume Fields -->
-    <?php do_action('submit_resume_form_resume_fields_start'); ?>
-
-    <?php foreach ($resume_fields as $key => $field) : ?>
-    <fieldset class="fieldset-<?php esc_attr_e($key); ?>">
-        <label
-            for="<?php esc_attr_e($key); ?>"><?php echo $field['label'] . apply_filters('submit_resume_form_required_label', $field['required'] ? ' <img class="requiredAsterisk" src=' . wp_get_attachment_url(13) . '>' : ' <small>' . __('(Recommended)', 'wp-job-manager-resumes') . '</small>', $field); ?></label>
-        <div class="field">
-            <?php $class->get_field_template($key, $field); ?>
-        </div>
-    </fieldset>
-    <?php endforeach; ?>
-
-    <?php do_action('submit_resume_form_resume_fields_end'); ?>
-
-    <p>
-        <input type="hidden" name="resume_manager_form" value="<?php echo $form; ?>" />
-        <input type="hidden" name="resume_id" value="<?php echo esc_attr($resume_id); ?>" />
-        <input type="hidden" name="job_id" value="<?php echo esc_attr($job_id); ?>" />
-        <input type="hidden" name="step" value="<?php echo esc_attr($step); ?>" />
-        <input type="submit" name="submit_resume" class="button" value="<?php esc_attr_e($submit_button_text); ?>" />
-    </p>
-
-    <?php else : ?>
-
-    <?php do_action('submit_resume_form_disabled'); ?>
-
-    <?php endif; ?>
-
-    <?php do_action('submit_resume_form_end'); ?>
+        <?php do_action('submit_resume_form_resume_fields_end'); ?>
+        <p>
+            <input type="hidden" name="resume_manager_form" value="<?php echo $form; ?>" />
+            <input type="hidden" name="resume_id" value="<?php echo esc_attr($resume_id); ?>" />
+            <input type="hidden" name="job_id" value="<?php echo esc_attr($job_id); ?>" />
+            <input type="hidden" name="step" value="<?php echo esc_attr($step); ?>" />
+            <input type="submit" name="submit_resume" class="button" value="<?php esc_attr_e($submit_button_text); ?>" />
+        </p>
+    <?php
+    else : do_action('submit_resume_form_disabled');
+    endif;
+    do_action('submit_resume_form_end'); ?>
 </form>
