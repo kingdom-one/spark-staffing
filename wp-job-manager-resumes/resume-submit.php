e1864e7578f4658f800580fa8e3a645413363d63
Joontae93 <kj.roelke@gmail.com>
2022-03-09 10:39:39 -0600

    img src url fix

--- wp-job-manager-resumes/resume-submit.php
+++ wp-job-manager-resumes/resume-submit.php
@@ -18,7 +18,8 @@ if (!defined('ABSPATH')) {
 
 wp_enqueue_script('wp-resume-manager-resume-submission');
 ?>
-<form action="<?php echo $action; ?>" method="post" id="submit-resume-form" class="job-manager-form" enctype="multipart/form-data">
+<form action="<?php echo $action; ?>" method="post" id="submit-resume-form" class="job-manager-form"
+    enctype="multipart/form-data">
     <?php do_action('submit_resume_form_start');
     if (apply_filters('submit_resume_form_show_signin', true)) {
         get_job_manager_template('account-signin.php', ['class' => $class], 'wp-job-manager-resumes', RESUME_MANAGER_PLUGIN_DIR . '/templates/');
@@ -29,22 +30,23 @@ wp_enqueue_script('wp-resume-manager-resume-submission');
         $resume_fields['candidate_education']['fields']['qualification']['label'] = 'Degree or Certification Name';
         do_action('submit_resume_form_resume_fields_start');
         foreach ($resume_fields as $key => $field) : ?>
-            <fieldset class="fieldset-<?php esc_attr_e($key); ?>">
-                <label for="<?php esc_attr_e($key); ?>"><?php echo $field['label'] . apply_filters('submit_resume_form_required_label', $field['required'] ? ' <img class="requiredAsterisk" src=' . wp_get_attachment_url(13) . '>' : ' <small>' . __('(Recommended)', 'wp-job-manager-resumes') . '</small>', $field); ?></label>
-                <div class="field">
-                    <?php $class->get_field_template($key, $field); ?>
-                </div>
-            </fieldset>
-        <?php endforeach; ?>
+    <fieldset class="fieldset-<?php esc_attr_e($key); ?>">
+        <label
+            for="<?php esc_attr_e($key); ?>"><?php echo $field['label'] . apply_filters('submit_resume_form_required_label', $field['required'] ? ' <img class="requiredAsterisk" src=' . wp_get_attachment_url(12) . '>' : ' <small>' . __('(Recommended)', 'wp-job-manager-resumes') . '</small>', $field); ?></label>
+        <div class="field">
+            <?php $class->get_field_template($key, $field); ?>
+        </div>
+    </fieldset>
+    <?php endforeach; ?>
 
-        <?php do_action('submit_resume_form_resume_fields_end'); ?>
-        <p>
-            <input type="hidden" name="resume_manager_form" value="<?php echo $form; ?>" />
-            <input type="hidden" name="resume_id" value="<?php echo esc_attr($resume_id); ?>" />
-            <input type="hidden" name="job_id" value="<?php echo esc_attr($job_id); ?>" />
-            <input type="hidden" name="step" value="<?php echo esc_attr($step); ?>" />
-            <input type="submit" name="submit_resume" class="button" value="<?php esc_attr_e($submit_button_text); ?>" />
-        </p>
+    <?php do_action('submit_resume_form_resume_fields_end'); ?>
+    <p>
+        <input type="hidden" name="resume_manager_form" value="<?php echo $form; ?>" />
+        <input type="hidden" name="resume_id" value="<?php echo esc_attr($resume_id); ?>" />
+        <input type="hidden" name="job_id" value="<?php echo esc_attr($job_id); ?>" />
+        <input type="hidden" name="step" value="<?php echo esc_attr($step); ?>" />
+        <input type="submit" name="submit_resume" class="button" value="<?php esc_attr_e($submit_button_text); ?>" />
+    </p>
     <?php
     else : do_action('submit_resume_form_disabled');
     endif;
